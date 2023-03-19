1118515ea8c466d39e2bf5a6e94aba7bdc1abc72
ttieman <tiemantanner@gmail.com>
2023-03-19 14:57:57 -0700

    implemented add role functionality

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,21 @@
 const mysql = require('mysql2');  // import sql 
 
 //Connect to sql
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'team_roster'
-});
 
-module.exports = db;
+const mysqlP = require('mysql2/promise');
+
+// Database connection configuration
+const connectionConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'team_roster',
+};
+
+// Initialize the connection variable
+const dbP = mysqlP.createConnection(connectionConfig);
+const db = mysql.createConnection(connectionConfig);
+
+
+module.exports = {db,
+                  dbP};
